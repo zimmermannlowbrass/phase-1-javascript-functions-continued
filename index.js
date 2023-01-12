d142aef586246420cbdc75afeb180b85516c553d
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-01-12 11:15:13 -0600

    completed labwork

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(fun = "roller-skate") {
+    return `This Saturday, I want to ${fun}!`;
+}
+
+function mondayWork(noFun = "go to the office") {
+    return `This Monday, I will ${noFun}.`;
+}
+
+function wrapAdjective(str = "*") {
+    const innerFunction = function (str2 = "special") {
+        return `You are ${str}${str2}${str}!`
+    }
+    return innerFunction;
+}
